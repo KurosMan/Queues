57531b0c81a61e4b81a7ed711a9920fb5d50adfa
U-KUROSUKE-PC\Kurosuke <Kurosuke@Kurosuke-PC.Kurosuke>
2020-05-19 12:01:44 +0100

    removed ready message

--- src/main/java/com/cgd/tutorials/RabbitAmqpTutorialsRunner.kt
+++ src/main/java/com/cgd/tutorials/RabbitAmqpTutorialsRunner.kt
@@ -14,7 +14,7 @@ class RabbitAmqpTutorialsRunner : CommandLineRunner {
 
     @Throws(Exception::class)
     override fun run(vararg arg0: String) {
-        println("Ready ... running for " + duration + "ms")
+        //println("Ready ... running for " + duration + "ms")
         Thread.sleep(duration.toLong())
         ctx!!.close()
     }
